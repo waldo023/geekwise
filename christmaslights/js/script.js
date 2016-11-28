b5cdfe162943c2e6ee34e5fc25efb39cc723f4cd
waldo023 <vallejoosvaldo12@yahoo.com>
2016-11-28 15:54:17 -0800

    project/nov28

---
+++ christmaslights/js/script.js
@@ -0,0 +1,89 @@
+function openImga() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('a').style.display = "block";
+}
+function closeImga(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('a').style.display = "none";
+}
+
+function openImgb() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('b').style.display = "block";
+}
+function closeImgb(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('b').style.display = "none";
+}
+
+function openImgc() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('c').style.display = "block";
+}
+function closeImgc(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('c').style.display = "none";
+}
+
+function openImgd() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('d').style.display = "block";
+}
+function closeImgd(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('d').style.display = "none";
+}
+
+function openImge() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('e').style.display = "block";
+}
+function closeImge(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('e').style.display = "none";
+}
+
+function openImgf() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('f').style.display = "block";
+}
+function closeImgf(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('f').style.display = "none";
+}
+
+function openImgg() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('g').style.display = "block";
+}
+function closeImgg(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('g').style.display = "none";
+}
+
+function openImgh() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('h').style.display = "block";
+}
+function closeImgh(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('h').style.display = "none";
+}
+
+function openImgi() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('i').style.display = "block";
+}
+function closeImgi(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('i').style.display = "none";
+}
+
+function openImgj() {
+  document.getElementById('overlay').style.display = "block";
+  document.getElementById('j').style.display = "block";
+}
+function closeImgj(){
+  document.getElementById('overlay').style.display = "none";
+  document.getElementById('j').style.display = "none";
+}
