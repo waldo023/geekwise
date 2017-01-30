121ebc1c077abd1b8da05aafbb18261fcd51c6a2
waldo023 <vallejoosvaldo12@yahoo.com>
2017-01-30 15:50:28 -0800

    jan30-js-variables

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,27 @@
+//alert('this is an alert')
+
+/*
+  comment
+  out
+  mulitple
+  lines
+*/
+
+var myNumberVar = 1, // Number Object
+    myFloatVar = 5.2,  // Float Object
+    myStringVar = "Osvaldo Vallejo", // String Object
+    myBooleanVar = false, // Boolean Object
+    myArrayVar = ['one', 'stingray', 'banana', 8, true], // Array Object
+    myObjectVar = {     //object object
+      make: 'Ford',
+      model: 'Focus',
+      color: 'beige',
+      year: 2009
+    };
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(myArrayVar);
+console.log(myObjectVar);
