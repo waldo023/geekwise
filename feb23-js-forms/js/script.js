bcc560a7636d64be84a47a62389b72531b10e035
waldo023 <vallejoosvaldo12@yahoo.com>
2017-02-24 15:34:47 -0800

    feb24-js-forms

--- feb23-js-forms/js/script.js
+++ feb23-js-forms/js/script.js
@@ -1,21 +1,62 @@
-var form = document.querySelector('form');
-var subBtn = document.querySelector('input[type="submit"]');
-var userArr = [];
-subBtn.addEventListener('click', function(evt){
+var contactForm = document.querySelector('form#contact');
+var loginForm = document.querySelector('form#login');
+var logBtn = document.querySelector('form#login input[type="submit"]');
+var conBtn = document.querySelector('form#contact input[type="submit"]');
+var nameSpan = document.querySelector('.custom');
+var conArr = [];
+var logArr = [];
+var username = '';
+
+if(localStorage.getItem('username')){
+  username = localStorage.getItem('username');
+  alert('please enter your password to continue.');
+  password();
+}else{
+  alert('please log in');
+  login();
+}
+console.log(username);
+
+conBtn.addEventListener('click', function(evt){
   evt.preventDefault();
-  for(var i = 0; i < form.elements.length - 1; i++){
-    // if(form.elements[i].type === 'text' ||
-    //    form.elements[i].type === 'textarea' ||
-    //    form.elements[i].type === 'email'){
-    //    console.log(form.elements[i].value);
-    // }
-    if(form.elements[i].type !== 'checkbox'){
-       userArr.push(form.elements[i].value);
+  for(var i = 0; i < contactForm.elements.length - 1; i++){
+    if(contactForm.elements[i].type !== 'checkbox' &&
+       contactForm.elements[i].value !== ''){
+      conArr.push(contactForm.elements[i].value);
     }
-    if(form.elements[i].type === 'checkbox' &&
-       form.elements[i].checked){
-         userArr.push(true);
+    if(contactForm.elements[i].type === 'checkbox' &&
+       contactForm.elements[i].checked){
+         conArr.push(true);
        }
   }
-  console.log(userArr);
+  console.log(conArr);
+  conArr = [];
 });
+
+function login(){
+logBtn.addEventListener('click', function(evt){
+  evt.preventDefault();
+  for(var i = 0; i < loginForm.elements.length - 1; i++){
+    if(loginForm.elements[i].type === 'text'){
+       localStorage.setItem('username', loginForm.elements[i].value)
+    }else{
+      sessionStorage.setItem('password', MD5(loginForm.elements[i].value));
+    }
+    nameSpan.textContent = 'Hello ' + localStorage.getItem('username') + '. ';
+    for(var i = 0; i < contactForm.length; i++){
+      contactForm.elements[i].disabled = false;
+    }
+  }
+});
+}
+
+function password(){
+  logBtn.addEventListener('click', function(evt){
+    evt.preventDefault();
+    if(MD5(loginForm.elements[1].value) === sessionStorage.getItem('password')){
+      alert('Passwords match. You may continue');
+    }else{
+      alert('please try again');
+    }
+  });
+}
